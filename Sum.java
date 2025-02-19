c6526919c39cb0929ed05125e47c8916dd17a812
Md Umar Faruk <mdumarfaruk793@gmail.com>
2025-02-19 23:33:07 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Sum{
+public static void main(String[] args){
+int a, b,sum;
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter first number : ");
+a = sc.nextInt();
+
+System.out.print("Enter second number : ");
+b= sc.nextInt();
+
+System.out.println();
+
+sum = a+b;
+
+System.out.println("Sum of the number :  " + sum);
+}
+}
